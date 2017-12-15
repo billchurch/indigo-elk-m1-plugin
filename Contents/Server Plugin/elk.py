ec57520eb62eb90b84afc4f809b9593d191c1283
billchurch <wmchurch@gmail.com>
2017-12-15 00:08:42 -0500

    initial release
    taking over abandoned plugin from
    https://sourceforge.net/projects/indigoelkm1plugin/files/

---
+++ Contents/Server Plugin/elk.py
@@ -0,0 +1,281 @@
+import telnetlib, time
+
+class Elk:
+
+	def __init__(self, host, port, timeout):
+		self.host = host
+		self.port = int(port)
+		self.timeout = int(timeout)
+		
+	def __del__(self):
+		self.conn.close()
+		
+	# Connect to alarm panel
+	def connect(self):
+		self.conn = telnetlib.Telnet(self.host, self.port)
+	
+	# Disconnect from alarm panel
+	def disconnect(self):
+		self.conn.close()
+
+	# Read data from alarm panel
+	def readData(self):
+		self.elkMsg = self.conn.read_until("\n", self.timeout)
+		return self.elkMsg
+		
+	# Send command to alarm panel
+	def sendCmd(self, cmd):
+		fullCmd = cmd + self.checksum(cmd) + "\n"
+#		print "Sending: %s" % fullCmd.rstrip()
+		self.conn.write(fullCmd)
+	
+	# Handle arming status (AS) message
+	def armStat(self, msg):
+		sarr = msg[4:12]
+		uarr = msg[12:20]
+		aarr = msg[20:28]
+
+		# Arming state
+		if sarr == '00000000':
+			arst = "Disarmed"
+		elif sarr == '10000000':
+			arst = "Armed_Away"
+		elif sarr == '20000000':
+			arst = "Armed_Stay"
+		elif sarr == '30000000':
+			arst = "Armed_Stay_Instant"
+		elif sarr == '40000000':
+			arst = "Armed_Night"
+		elif sarr == '50000000':
+			arst = "Armed_Night_Instant"
+		elif sarr == '60000000':
+			arst = "Armed_Vacation"
+		else:
+			arst = "Unknown"
+
+		# Arming readiness
+		if uarr == '01111111':
+			armrd = "Not_Ready"
+		elif uarr == '11111111':
+			armrd = "Ready"
+		else:
+			armrd = "Armed"
+
+		# Alarmed state?
+			# try, except for non integer values
+			# TODO - handle all different alarm states
+		try:
+			zonealm = int(aarr[0:1])
+		except:
+			zonealm = 0
+			
+		if zonealm == 0:
+			almst = "No_Alarm"
+		elif zonealm == 1:
+			almst = "Entry_Delay"
+		else:
+			almst = "Alarm_Tripped"
+
+		return arst, armrd, almst
+
+	# Handle enter exit delay state (EE) message
+	def eeStat(self, msg):
+		eeType = msg[5:6]
+
+		if eeType == '0':
+			eeMsg = "Exit"
+		elif eeType == '1':
+			eeMsg = "Entry"
+		else:
+			eeMsg = "No Delay"
+			
+		return eeMsg
+
+	# Handle string data (SD) message
+	def stringData(self, msg):
+		if msg[4:6] == '00':
+			dtype = "z"
+#			dtype = "Zone"
+		elif msg[4:6] == '01':
+			dtype = "a"
+#			dtype = "Area"
+		elif msg[4:6] == '11':
+			dtype = "t"
+#			dtype = "Thermostat"
+		else:
+			dtype = "o"
+#			dtype = "Other"
+			
+		id = msg[6:9]
+		name = msg[9:25]
+		
+		return dtype, id, name
+
+	# Handle lighting (PC) message
+	def lights(self, msg):
+		
+		
+		zone = msg[4:7]
+		status = msg[7:9]
+			
+		return zone, status
+	# Handle thermostat report (TR) message
+	def thermoRpt(self, msg):
+		tnum = int(msg[4:6])
+		temp = msg[9:11]
+		heat = msg[11:13]
+		cool = msg[13:15]
+		
+		if 	msg[6:7] == '0':
+			mode = "Off"
+		elif msg[6:7] == '1':
+			mode = "Heat"
+		elif msg[6:7] == '2':
+			mode = "Cool"
+		elif msg[6:7] == '3':
+			mode = "Auto"
+		elif msg[6:7] == '4':
+			mode = "Emer-Heat"
+		else:
+			mode = "Unknown"
+			
+		if msg[7:8] == '0':
+			hold = "False"
+		else:
+			hold = "True"
+			
+		if msg[8:9] == '0':
+			tfan = "Auto"
+		else:
+			tfan = "On"
+			
+		return tnum, mode, hold, tfan, temp, heat, cool
+
+	# Handle zone change (ZC) message
+	def zoneChange(self, msg):
+		zone = int(msg[4:7])
+		stat = int(msg[7:8], 16)
+		zoneS = []
+		
+		if 0 < stat < 4:
+			status = "Normal"
+		elif 4 < stat < 8:
+			status = "Trouble"
+		elif 8 < stat < 12:
+			status = "Violated"
+		else:
+			status = "Unknown"
+		
+		zoneS.append((zone, status))
+		return zoneS
+
+	# Handle zone data (ZD) message
+	def zoneData(self, msg):
+		allZones = msg[4:-4]
+		useZones = []
+		for lctr in range(0,len(allZones)):
+			if allZones[lctr] != '0':
+				if allZones[lctr] == '1':
+					zoneType = "Burglar Entry/Exit 1"
+				elif allZones[lctr] == '2':
+					zoneType = "Burglar Entry/Exit 2"
+				elif allZones[lctr] == '3':
+					zoneType = "Burglar Perimeter Instant"
+				elif allZones[lctr] == '4':
+					zoneType = "Burglar Interior"
+				elif allZones[lctr] == '5':
+					zoneType = "Burglar Interior Follower"
+				elif allZones[lctr] == '6':
+					zoneType = "Burglar Interior Night"
+				elif allZones[lctr] == '7':
+					zoneType = "Burglar Interior Night Delay"
+				elif allZones[lctr] == '8':
+					zoneType = "Burglar 24 Hour"
+				elif allZones[lctr] == '9':
+					zoneType = "Burglar Box Tamper"
+				elif allZones[lctr] == ':':
+					zoneType = "Fire Alarm"
+				elif allZones[lctr] == ';':
+					zoneType = "Fire Verified"
+				elif allZones[lctr] == '<':
+					zoneType = "Fire Supervisory"
+				elif allZones[lctr] == '=':
+					zoneType = "Aux Alarm 1"
+				elif allZones[lctr] == '>':
+					zoneType = "Aux Alarm 2"
+				elif allZones[lctr] == '?':
+					zoneType = "Keyfob"
+				elif allZones[lctr] == '@':
+					zoneType = "Non Alarm"
+				elif allZones[lctr] == 'A':
+					zoneType = "Carbon Monoxide"
+				elif allZones[lctr] == 'B':
+					zoneType = "Emergency Alarm"
+				elif allZones[lctr] == 'C':
+					zoneType = "Freeze Alarm"
+				elif allZones[lctr] == 'D':
+					zoneType = "Gas Alarm"
+				elif allZones[lctr] == 'E':
+					zoneType = "Heat Alarm"
+				elif allZones[lctr] == 'F':
+					zoneType = "Medical Alarm"
+				elif allZones[lctr] == 'G':
+					zoneType = "Police Alarm"
+				elif allZones[lctr] == 'H':
+					zoneType = "Police No Indication"
+				elif allZones[lctr] == 'I':
+					zoneType = "Water Alarm"
+				elif allZones[lctr] == 'J':
+					zoneType = "Key Momentary Arm / Disarm"
+				elif allZones[lctr] == 'K':
+					zoneType = "Key Momentary Arm Away"
+				elif allZones[lctr] == 'L':
+					zoneType = "Key Momentary Arm Stay"
+				elif allZones[lctr] == 'M':
+					zoneType = "Key Momentary Disarm"
+				elif allZones[lctr] == 'N':
+					zoneType = "Key On / Off"
+				elif allZones[lctr] == 'O':
+					zoneType = "Mute Audibles"
+				elif allZones[lctr] == 'P':
+					zoneType = "Power Supervisory"
+				elif allZones[lctr] == 'Q':
+					zoneType = "Temperature"
+				elif allZones[lctr] == 'R':
+					zoneType = "Analog Zone"
+				elif allZones[lctr] == 'S':
+					zoneType = "Phone Key"
+				elif allZones[lctr] == 'T':
+					zoneType = "Intercom Key"
+
+				useZones.append(((lctr + 1), zoneType))
+
+		return useZones
+
+	# Handle zone status (ZS) message
+	def zoneStat(self, msg):
+		allZones = msg[4:-4]
+		useZones = []
+		for lctr in range(0,len(allZones)):
+			if int(allZones[lctr], 16) != 0:
+				if 0 < int(allZones[lctr],16) < 4:
+					status = "Normal"
+				elif 4 < int(allZones[lctr],16) < 8:
+					status = "Trouble"
+				elif 8 < int(allZones[lctr],16) < 12:
+					status = "Violated"
+				else:
+					status = "Unknown"
+					
+				useZones.append(((lctr + 1), status))
+
+		return useZones
+
+	# Calculate checksum of command
+	def checksum(self, cmd):
+		summ = 0
+		for item in range(len(cmd)):
+			summ += ord(cmd[item])
+		chksum = "%02X\n" % (summ * -1 % 256)
+		return chksum
+
---
+++ Contents/Server Plugin/elkextra.py
@@ -0,0 +1,161 @@
+#
+#
+# Extra stuff for the ELK plugin
+#
+#
+
+import indigo
+
+# create ELK M1 folder and return folderId
+def createFolder(pluginFolder):
+	try:
+		folderId = indigo.devices.folders["ELK M1"].id
+	except KeyError, e:
+		if e.message == "key name ELK M1 not found in database":
+			if pluginFolder:
+				indigo.devices.folder.create("ELK M1")
+				folderId = indigo.devices.folders["ELK M1"].id
+			else:
+				folderId = 0
+	
+	return folderId
+
+# create Alarm Panel device and return panelId
+def createPanel(autoPanel, ip_addr, ip_port, folderId):
+	try:
+		panelId = indigo.devices["Alarm Panel"].id
+	except KeyError, e:
+		if e.message == "key name Alarm Panel not found in database":
+			if autoPanel:
+				addr = "%s:%s" % (ip_addr, ip_port)
+				pDev = indigo.device.create(protocol=indigo.kProtocol.Plugin,
+					address=addr,
+					name="Alarm Panel",
+					description="ELK M1 Alarm Panel",
+					pluginId="com.elkplug.indigoplugin.elkm1g",
+					deviceTypeId="alarmPanel",
+					props={"address":addr, "ip_address":ip_addr, "ip_port":ip_port},
+					folder=folderId)
+				panelId = pDev.id
+			else:
+				panelId = 0
+
+	return panelId
+
+# process 'AS' response to set arming status
+def setArmingStatus(arst, armrd, almst, panelId):
+	indigo.devices[panelId].updateStateOnServer("arm_state", arst)
+	indigo.devices[panelId].updateStateOnServer("arm_ready", armrd)
+	indigo.devices[panelId].updateStateOnServer("alarm_state", almst)
+
+# process 'PC" response and set status of the corresponding device
+def setDeviceLight(zone, lightmsg):
+	for device in indigo.devices:
+	
+		if device.address == zone:
+			if lightmsg == "00":
+				indigo.device.turnOff(device)
+			elif lightmsg == "01":
+				indigo.device.turnOn(device)
+			
+			
+# process 'SD' response and set description of indigo device
+def setDeviceDesc(eaddr, eid, ename, autoNames):
+	if autoNames:
+		if eaddr == 'z':
+			etype = "Zone"
+		elif eaddr == 'a':
+			etype = "Area"
+		elif eaddr == 't':
+			etype = "Thermostat"
+		else:
+			etype = "Other"
+	
+		devAddr = "%s%s" % (eaddr, eid)
+			
+		for device in indigo.devices:
+			if device.address == devAddr:
+				device.name = ename
+				device.replaceOnServer()
+
+# process 'TR' response for thermostats
+def setThermoInfo(tnum, mode, hold, tfan, temp, heat, cool, folderId):
+	tdevNm = "Thermostat %s" % str(tnum)
+	addr = "t%03d" % tnum
+	existaddrs = []
+	existdevs = []
+
+	for device in indigo.devices:
+		if device.deviceTypeId == 'alarmTstat':
+			existaddrs.append(device.address)
+			existdevs.append(device)
+
+	if addr in existaddrs:
+		for device in existdevs:
+			if addr == device.address:
+				device.updateStateOnServer("mode", mode)
+				device.updateStateOnServer("temp_hold", hold)
+				device.updateStateOnServer("fan_mode", tfan)
+				device.updateStateOnServer("curr_temp", temp)
+				device.updateStateOnServer("heat_setpoint", heat)
+				device.updateStateOnServer("cool_setpoint", cool)
+	else:
+		updateDev = indigo.device.create(protocol=indigo.kProtocol.Plugin,
+			address="",
+			name=tdevNm,
+			description="",
+			pluginId="com.elkplug.indigoplugin.elkm1g",
+			deviceTypeId="alarmTstat",
+			props={"address":addr},
+			folder=folderId)
+		updateDev.updateStateOnServer("mode", mode)
+		updateDev.updateStateOnServer("temp_hold", hold)
+		updateDev.updateStateOnServer("fan_mode", tfan)
+		updateDev.updateStateOnServer("curr_temp", temp)
+		updateDev.updateStateOnServer("heat_setpoint", heat)
+		updateDev.updateStateOnServer("cool_setpoint", cool)
+
+# process 'ZD' response to request for zone data
+def setupZones(zoneD, folderId, autoNames):
+	existaddrs = []
+	existdevs = []
+	for device in indigo.devices:
+		if device.deviceTypeId == 'alarmZone':
+			existaddrs.append(device.address)
+			existdevs.append(device)
+
+	for zone in zoneD:
+		zdAddr = "z%03d" % zone[0]
+		zdName = "Zone %s" % zone[0]
+
+		if zdAddr in existaddrs:
+			for device in existdevs:
+				if zdAddr == device.address:
+					indigo.server.log("Updating existing device %s, %s" % (device.name, zone[1]))
+					device.replacePluginPropsOnServer({"address":zdAddr, "type":zone[1]})
+					if autoNames:
+						device.description = zone[1]
+						device.replaceOnServer()
+		else:
+			try:
+				indigo.device.create(protocol=indigo.kProtocol.Plugin,
+					address="",
+					name=zdName,
+					description="",
+					pluginId="com.elkplug.indigoplugin.elkm1g",
+					deviceTypeId="alarmZone",
+					props={"address":zdAddr,"type":zone[1]},
+					folder=folderId)
+				indigo.server.log("Creating device - Name: %s, Zone #: %s" % (zdName, zdAddr))
+			except ValueError:
+				# don't create device that already exists
+				pass
+
+# process 'ZC' or 'ZS' responses to set zone status
+def setZoneStatus(zoneS):
+	for zone in zoneS:
+		devAddr = "z%03d" % zone[0]
+		for device in indigo.devices:
+			if device.address == devAddr:
+				device.updateStateOnServer("status", zone[1])
+
